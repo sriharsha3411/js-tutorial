ab17663899f923c562d6854a52c63392d307dff1
sriharsha3411 <155721612+sriharsha3411@users.noreply.github.com>
2025-04-27 14:40:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict";  // makes the code safer and catches the errors easily
+                    //   x=5;
+                    //   console.log(x) //error: x not defined
+                                        // if we dont use "use strict"  output=5
+
+//alert( 3+3 )  // we are using nodejs, not browser 
+
+
+let username = "Harsha"         //string 
+let age = 22              //number
+let isLoggedIn = false      //boolean
+let state;                  //undefined
+let marks = null            //null
+
+
+// number => range(2 to power 53)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => value not assigned 
+// symbol => used for uniqueness
+
+//object
+
+
+console.log(typeof "Harsha");
+console.log(typeof username);
+console.log(typeof (age))
+console.log(typeof isLoggedIn)
+
+
+console.log(typeof null);       //object
+console.log(typeof undefined);  //undefined
+
+
+
