83e3895406513c1475065ecf309fcb562a104a81
sriharsha3411 <155721612+sriharsha3411@users.noreply.github.com>
2025-04-27 15:06:57 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "10"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+/*
+"10" => 10
+"10abc" => NaN
+true => 1, false => 0
+null => 0
+undefined => NaN
+*/
+
+
+
+let isLoggedIn = 1
+
+let booleanInLoggedIn = Boolean(isLoggedIn)
+console.log(booleanInLoggedIn)
+
+/*
+1 => true, 0 => false
+"" => false
+"harsha" => true
+*/
+
+
+let num = 10
+
+let stringNumber = String(num)
+console.log(typeof stringNumber);
+console.log(stringNumber)
