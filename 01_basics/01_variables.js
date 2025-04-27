e2095768ba7bb00a1a96dad39896af54290f3c11
sriharsha3411 <155721612+sriharsha3411@users.noreply.github.com>
2025-04-27 07:36:44 +0000

    Added 01_variables.js file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345;
+let accountEmail = "harsha@gmail.com";
+var accountPassword = "45678"
+
+let accountState;
+
+// console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountState]);
