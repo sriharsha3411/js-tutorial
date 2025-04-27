0ca2381ab650790099c7437ade879259b31ac6a7
sriharsha3411 <155721612+sriharsha3411@users.noreply.github.com>
2025-04-27 14:40:17 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,28 @@
 const accountId = 12345;
 let accountEmail = "harsha@gmail.com";
-var accountPassword = "45678"
+var accountPassword = "45678";
+accountName = "Harsha";
 
 let accountState;
 
-// console.log(accountEmail);
+console.log(accountId);
+
+//accountId = 2  // not allowed because it is declared as const
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+
+console.log(accountEmail,accountPassword);
+
+
+accountEmail = "sunny@gmail.com";
+accountPassword = "77024"
+accountName = "Sunny"
+
+
+
 console.table([accountId, accountEmail, accountPassword, accountState]);
